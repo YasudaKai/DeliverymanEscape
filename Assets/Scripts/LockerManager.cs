b3e9bf90b120ab604f74727836d449dfa54e5c8d
kai.yasuda <kai0603603@gmail.com>
2021-01-19 23:36:55 +0900

    ChangeLockerDial

--- Assets/Scripts/LockerManager.cs
+++ Assets/Scripts/LockerManager.cs
@@ -2,27 +2,18 @@
 using System.Collections.Generic;
 using System.IO.MemoryMappedFiles;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class LockerManager : MonoBehaviour
 {
     public GameObject wall;
     public GameObject[] wallButtons;
-    public WallManager wallManager;
+    public Image[] dials;//現在の画像
+    public Sprite[] changeDials;//変更後の画像
     public const int left = 0;
     public const int right = 1;
     public const int back = 2;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     //ロッカーの鍵に当たり判定をつけて
     //いつ：ロッカー鍵付近をクリックした時
@@ -36,10 +27,15 @@ public class LockerManager : MonoBehaviour
     /// 処理：左右の方向きーを削除。バックボタンを追加。
     public void OnLockerButton()
     {
-        var wPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var hit = Physics2D.Raycast(wPos, Vector2.zero);
+        Vector3 wPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //マウスをクリックしたワールド座標をを取得
+        
+        RaycastHit2D hit = Physics2D.Raycast(wPos, Vector2.zero);
+        //wPosが２D空間上のRayの原点。
+        //そこからどの方向ににRayをあてるか
+
         if (hit)
-        {
+        {　　　
             if (hit.collider.gameObject.name == this.name)
             {
                 Debug.Log("Lockerのダイヤルに当たった。");
@@ -53,6 +49,68 @@ public class LockerManager : MonoBehaviour
             }
         }
     }
+
+    //DialButtonをプッシュしたら、画像を変更したい
+    //いつ：DialButtonをプッシュした時
+    // →どのボタンを押したのか引数で区別する ok
+   
+    //処理：画像を変更したい
+    //→現在の画像を取得
+    //→変更後の画像を取得
+    //→マーク変数で現在の画像を管理
+    //→ボタンが押されるたびにマーク変数を＋＋していき
+    //その値に応じてSpriteで画像を変更する
+
+
+    public enum Dial
+    {
+        Boar,//イノシシ
+        Mouse,
+        Cat,
+        Dog,
+    }
+
+    Dial currentDial = Dial.Dog;
+    public void OnDialButton(int index)
+    {
+        Debug.Log(index + "dialPush!!");
+        currentDial++;
+        if(currentDial > Dial.Dog)
+        {
+            currentDial = Dial.Boar;
+        }
+
+        ChangeDialButton(index);
+    }
+
+    void ChangeDialButton(int index)
+    {
+        dials[index].sprite = GetDialImage();
+    }
+
+    //マーク変数の値によって
+    //返す画像を決める。
+    Sprite GetDialImage()
+    {
+        switch (currentDial)
+        {
+            case Dial.Boar:
+                return changeDials[0];
+                break;
+            case Dial.Mouse:
+                return changeDials[1];
+                break;
+            case Dial.Cat:
+                return changeDials[2];
+                break;
+            case Dial.Dog:
+                return changeDials[3];
+                break;
+        }
+        return null;
+    }
+
+
 
     
 }
